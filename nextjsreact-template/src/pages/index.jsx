54df92c5133cf208ef9b7a56edd63b37959a9000
Brandon Berrios <140827644+brandonberrios@users.noreply.github.com>
2024-03-05 00:14:30 +0000

    Updated package.json

--- nextjsreact-template/src/pages/index.jsx
+++ nextjsreact-template/src/pages/index.jsx
@@ -1,16 +1,10 @@
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
+import { Jumbotron } from "@/components/Jumbotron";
 
 export default function Home() {
   return (
-   <Container>
-    <Row>
-      <Col>
-        <h1>My own react template</h1>
-
-        <Button variant="primary">Click Here!</Button>
-      </Col>
-    </Row>
-   </Container>
-
-  )
+    <Container>
+      <Jumbotron />
+    </Container>
+  );
 }
